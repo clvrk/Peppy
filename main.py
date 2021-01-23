9fa8a62e3045f58e5ceb39056d92c6dd855c5d85
clvrk <jheru711@Gmail.com>
2021-01-23 17:45:11 -0400

    Finishing touches for setup.

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from discord.ext import commands
 import os
 from dotenv import load_env
 
-
 load_env()
 
-
 bot = commands.AutoShardedBot(command_prefix=True, help_command=None, case_insensitive=True)
+
+colors = [0xFFFF33, 0xADFF2F]
 
 @bot.event
 async def on_ready():
     print(f"{bot} is online")
 
+
+extensions = ['Commands.music']
+
+
+if __name__ == '__main__':
+    for ext in extensions:
+        bot.load_extension(ext)
 
 
 bot.run(os.getenv("TOKEN"))
