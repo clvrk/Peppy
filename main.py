4b9693cbee74acd95cda056bcbf524e64e95dec2
clvrk <jheru711@Gmail.com>
2021-01-23 14:44:15 -0400

    fml

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_env
+
+
+load_env()
+
+
+bot = commands.AutoShardedBot(command_prefix=True, help_command=None, case_insensitive=True)
+
+@bot.event
+async def on_ready():
+    print(f"{bot} is online\n\n{bot} is in {len(bot.guilds)} servers\n\n{len(bot.users)} users")
+
+
+
+bot.run(os.getenv("TOKEN"))
