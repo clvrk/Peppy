8902351ba6fdcbd3c9021afcef2b797c04234540
TahaMori <74666116+LetsChill@users.noreply.github.com>
2021-01-24 18:53:16 +0200

    Added Uptime command As a start for devtools.py

--- Commands/devtools.py
+++ Commands/devtools.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
+import time
+from datetime import datetime
 
-
+#variable for the bot to know when it started
+starttime = time.time()
 
 
 
@@ -11,15 +14,17 @@ class Devtools(commands.Cog):
         self.bot = bot
 
 
+    @command.commands()
+    async def uptime(self, ctx)
+        #Delta func and more functions to calculate and convert to more neat format
 
-
-
-
-
-
-
-
-
+        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        up = discord.Embed(title="The Bot Uptime Is:", description=f"`{seconds}s, {minutes}m, {hours}h, {days}d\n`", color = 0xe67e22)
+        ctx.send(embed=up)
+     
 
 
 def setup(bot):
