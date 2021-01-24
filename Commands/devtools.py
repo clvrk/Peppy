5825edcba3e7ea653fc22e38f5c4d1953051c492
TahaMori <74666116+LetsChill@users.noreply.github.com>
2021-01-24 18:38:26 +0200

    Added devtools cog

---
+++ Commands/devtools.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+
+
+
+
+
+class Devtools(commands.Cog):
+   
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Devtools(bot))
